32418065abb7e489c24e2556a1bdefc20061efdf
MarahDeven <marahdeven@gmail.com>
2022-05-11 10:00:32 -0400

    created navigation bar with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
